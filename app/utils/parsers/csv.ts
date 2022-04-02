ed0cb6e8a954127cc99690ab2147e47367c34644
amaralc <amaral.calil@gmail.com>
2022-04-02 14:24:46 -0300

    fix: adjust csv parser

--- app/utils/parsers/csv.ts
+++ app/utils/parsers/csv.ts
@@ -1,10 +1,40 @@
+/**
+ * @see https://stackoverflow.com/questions/8493195/how-can-i-parse-a-csv-string-with-javascript-which-contains-comma-in-data
+ */
+
+// Return array of string values, or NULL if CSV string not well formed.
+export function parseCsvString(text) {
+  var re_valid =
+    /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
+  var re_value =
+    /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
+  // Return NULL if input string is not well formed CSV string.
+  if (!re_valid.test(text)) return null;
+  var a = []; // Initialize array to receive values.
+  text.replace(
+    re_value, // "Walk" the string using replace with callback.
+    function (m0, m1, m2, m3) {
+      // Remove backslash from \' in single quoted values.
+      if (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+      // Remove backslash from \" in double quoted values.
+      else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
+      else if (m3 !== undefined) a.push(m3);
+      return ''; // Return empty string.
+    },
+  );
+  // Handle special case of empty last value.
+  if (/,\s*$/.test(text)) a.push('');
+  return a;
+}
+
 export const parseCsv = (data) => {
-  console.log(`csv`);
+  console.log(`csv`, data);
   let parsedata = [];
 
   let newLinebrk = data.split('\n');
+
   for (let i = 0; i < newLinebrk.length; i++) {
-    parsedata.push(newLinebrk[i].split(','));
+    parsedata.push(parseCsvString(newLinebrk[i]));
   }
 
   console.table(parsedata);
